66475af7c5a3a68f425416f67d6e53bcb5fe8ba1
benner.dias <bennerheberth@gmail.com>
2025-10-02 14:19:05 -0300

    Implementação da camada de segurança, modulo Auth

---
+++ src/auth/constants/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: 'X7eTMUhJxxO9ppVMknUWJNrT7czQXwhHIKRwbYnZ4EZ77vGTEgk9UlNzpf/SINV6',
+};
--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriaController } from './controllers/categoria.controller';
+import { CategoriaService } from './services/categoria.service';
+import { Categoria } from './entities/categoria.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Categoria]), CategoriaModule],
