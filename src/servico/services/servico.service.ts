cb41b108a30c2097b7514917a4321ccc964358f6
unknown <valerde88@outlook.com>
2025-10-02 11:18:43 -0300

    Criação da pasta service

---
+++ src/servico/services/servico.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Servico } from '../entities/servico.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ServicoService {
+  constructor(
+    @InjectRepository(Servico)
+    private servicoRepository: Repository<Servico>,
+  ) {}
+}
