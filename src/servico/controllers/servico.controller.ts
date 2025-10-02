924d34975315f20b6669c6b49894a4c2cd46f3d4
unknown <valerde88@outlook.com>
2025-10-02 14:55:14 -0300

    criação de metodos e atualizações dos modulos

--- src/servico/controllers/servico.controller.ts
+++ src/servico/controllers/servico.controller.ts
@@ -1,27 +1,48 @@
+import { ServicoService } from './../services/servico.service';
+import { Servico } from '../entities/servico.entity';
 import {
+  Body,
   Controller,
   Get,
   HttpCode,
   HttpStatus,
   Param,
   ParseIntPipe,
+  Post,
+  Put,
 } from '@nestjs/common';
-import { ServicoService } from '../services/servico.service';
-import { Servico } from '../entities/servico.entity';
 
 @Controller('/servicos')
 export class ServicoController {
-  constructor(private readonly servicoController: ServicoService) {}
+  constructor(private readonly servicoService: ServicoService) {}
 
   @Get()
   @HttpCode(HttpStatus.OK)
   findAll(): Promise<Servico[]> {
-    return this.servicoController.findAll();
+    return this.servicoService.findAll();
   }
 
   @Get('/:id')
   @HttpCode(HttpStatus.OK)
   findById(@Param('id', ParseIntPipe) id: number): Promise<Servico> {
-    return this.servicoController.finfById(id);
+    return this.servicoService.finfById(id);
+  }
+
+  @Get('/destino/:destino')
+  @HttpCode(HttpStatus.OK)
+  findAllByDestino(@Param('titulo') titulo: string): Promise<Servico[]> {
+    return this.servicoService.findAllByDestino(titulo);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() servico: Servico): Promise<Servico> {
+    return this.servicoService.create(servico);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() servico: Servico): Promise<Servico> {
+    return this.servicoService.update(servico);
   }
 }
--- src/servico/services/servico.service.ts
+++ src/servico/services/servico.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Servico } from '../entities/servico.entity';
-import { Repository } from 'typeorm';
+import { ILike, Repository } from 'typeorm';
 
 @Injectable()
 export class ServicoService {
@@ -25,5 +25,23 @@ export class ServicoService {
       throw new HttpException('Postagem não encontrada', HttpStatus.NOT_FOUND);
 
     return servico;
+  }
+
+  async findAllByDestino(destino: string): Promise<Servico[]> {
+    return await this.servicoRepository.find({
+      where: {
+        destino: ILike(`%${destino}%`),
+      },
+    });
+  }
+
+  async create(servico: Servico): Promise<Servico> {
+    return await this.servicoRepository.save(servico);
+  }
+
+  async update(servico: Servico): Promise<Servico> {
+    await this.finfById(servico.id);
+
+    return await this.servicoRepository.save(servico);
   }
 }
